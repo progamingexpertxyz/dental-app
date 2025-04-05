c8860cdfde4dceb7d0be07c12db4283740be1dc3
Tanzeel Naveed Khan 💻 <tanzeelnaveed8@gmail.com>
2025-04-05 10:56:35 +0500

    Update appointment.ts

--- app/models/appointment.ts
+++ app/models/appointment.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const AppointmentSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+   name: { type: String, required: true },
   email: { type: String, required: true },
-  date: { type: String, required: true },
-  time: { type: String, required: true },
+  number: { type: String, required: true },
+  age: { type: String, required: true },  
   service: { type: String, required: true },
 });
 
