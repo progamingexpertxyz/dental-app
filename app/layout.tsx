0e3b336954333d5d5e76b1dfd9601a1f68e7e341
Tanzeel Naveed Khan 💻 <tanzeelnaveed8@gmail.com>
2025-04-15 11:37:38 +0500

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import SessionWrapper from "@/app/components/SessionWrapper"; // ✅ Import SessionWrapper
-
+import InstallButton from "./components/install";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
