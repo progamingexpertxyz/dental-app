afedb800aebbf752a4f18bd600107a92d0bd0b21
Tanzeel Naveed Khan 💻 <tanzeelnaveed8@gmail.com>
2025-04-15 16:49:27 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 // next.config.js
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
 });
 
-module.exports = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-});
+};
+
+module.exports = withPWA(nextConfig);
