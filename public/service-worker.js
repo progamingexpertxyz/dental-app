dfbcb9a86ca685532af3a73f100ba0cc4cebcf0e
Tanzeel Naveed Khan 💻 <tanzeelnaveed8@gmail.com>
2025-04-15 22:10:27 +0500

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+// public/service-worker.js
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
