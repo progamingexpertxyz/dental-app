bc4d6e02a403b2d114bead817b10ce7f6fed632c
Tanzeel Naveed Khan 💻 <tanzeelnaveed8@gmail.com>
2025-04-16 09:18:17 +0500

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  // Clean old caches
-  buildExcludes: [/middleware-manifest\.json$/],
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
-  // Add your other configs
 });
