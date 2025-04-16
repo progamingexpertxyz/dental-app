3187361d63ec0a9fc9e29836640eaa71698282a8
Tanzeel Naveed Khan 💻 <tanzeelnaveed8@gmail.com>
2025-04-16 09:33:25 +0500

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Optional: add more config here
 }
 
 export default nextConfig
